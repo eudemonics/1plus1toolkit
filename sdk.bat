049bce691c9ba05ab0277afd099e2a34726da035
eudemonics <vvn@eudemonics.org>
2015-05-08 17:26:16 -0700

    updated scripts for setting path and installing android tools

---
+++ sdk.bat
@@ -0,0 +1,13 @@
+@echo OFF
+
+set OPO_HOME=%CD%
+set PATH=%PATH%;%CD%;
+
+del adb
+del adb-linux
+del fastboot
+del fastboot-linux
+
+icacls adb.exe /grant %USERNAME%:F
+icacls Adb*.dll /grant %USERNAME%:F
+icacls fastboot.exe /grant %USERNAME%:F
